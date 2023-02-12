8e4aff7152547a8e02638ed8aaf08cafca0fb1b4
prudhvigana <122306094+prudhvigana@users.noreply.github.com>
2023-02-12 16:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
-
+function randomNumberGenerator() {
+	
+	document.getElementById("num").innerHTML = Math.floor((Math.random() * 20) + 1);
+	var numberGenerated = document.getElementById("num").innerHTML;
+	var guessedValue = document.getElementById("guess").value;
+	if ((Math.abs(numberGenerated-guessedValue)<=5))
+	{
+		document.getElementById("respond").innerHTML = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+	
+}
